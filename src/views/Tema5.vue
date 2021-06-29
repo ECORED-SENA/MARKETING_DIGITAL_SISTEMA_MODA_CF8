a4e118234c058c10e1e43e57a0c54c0740a09e7e
Luis Urueta <lgurueta@misena.edu.co>
2021-06-29 12:42:22 -0500

    avance3

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -3,6 +3,146 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
+    .titulo-principal
+      .titulo-principal__numero
+        span 5
+      h1 Análisis e identificación de la estructura del cuerpo y medidas anatómicas
+    
+    figure.mb-4
+      img(src="@/assets/curso/tema5/img01.png", alt="cuerpo y medidas anatómicas")
+    
+    p.mb-4 El vestuario está diseñado para dar cobertura al cuerpo, es por ello, que analizar el cuerpo entorno a su estructura y movimiento es de suma importancia para comprender a qué se debe el patronaje y las diferentes metodologías utilizadas en su realización. Para el desarrollo de prendas de vestuario sobre medidas se hace aún más taxativo comprenderlo pues como técnicos en el área se enfrentarán a diversas siluetas que necesitan ser analizadas dentro de la metodología y el paso a paso que se determina para la elaboración de un patrón.
+
+    p.mb-5 Teniendo en cuenta lo anterior, se hará un recorrido para lograr entender el cuerpo y su estructura, a partir de los siguientes conceptos en relación con la estructura corporal:
+
+    .row.justify-content-center
+      .col-lg-10
+        TabsA.color-primario.mb-4
+          .tarjeta.bg011.p-4(titulo="Anatomía")
+            .row.align-items-end
+              .col-lg-8.mb-4
+                p.mb-0 Ciencia que estudia toda estructura de los seres vivos, incluido su topografía, disposición y la relacsión de su cuerpo y los órganos que lo integran.
+              .col-lg-4
+                figure
+                  img(src="@/assets/curso/tema5/img02.svg", alt="Anatomía")
+          
+          .tarjeta.bg011.p-4(titulo="Biomecánica")
+            .row.align-items-end
+              .col-lg-8.mb-4
+                p.mb-0 Es la conjunción de disciplinas que estudian la estructura, función y movimiento del cuerpo, utiliza métodos mecánicos para determinar su fuerza y desplazamiento.
+              .col-lg-4
+                figure
+                  img(src="@/assets/curso/tema5/img03.svg", alt="Biomecánica")
+          
+          .tarjeta.bg011.p-4(titulo="Morfología")
+            .row.align-items-end
+              .col-lg-.mb-4
+                p.mb-0 Concepto relacionado a la parte biológica, encargada de estudiar la forma de los seres vivos.
+              .col-lg-4
+                figure
+                  img(src="@/assets/curso/tema5/img04.svg", alt="Morfología")
+          
+          .tarjeta.bg011.p-4(titulo="Ergonomía")
+            .row.align-items-end
+              .col-lg-8.mb-4
+                p.mb-0 Disciplina que estudia las interacciones del ser humano con su entorno, reúne información que permite medir el desempeño y la optimización, buscando el bienestar de las personas a través del vestuario.
+              .col-lg-4
+                figure
+                  img(src="@/assets/curso/tema5/img05.svg", alt="Ergonomía")
+
+          .tarjeta.bg011.p-4(titulo="Antropometría")
+            .row.align-items-end
+              .col-lg-8.mb-4
+                p.mb-0 Estudia a nivel cuantitativo las características físicas de las personas, es decir, todo lo relacionado con medidas y proporciones del cuerpo humano.
+              .col-lg-4
+                figure
+                  img(src="@/assets/curso/tema5/img06.svg", alt="Antropometría")
+    
+        .bloque-texto-a.color-secundario.modBloque.p-4.p-md-5.mb-4
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-8.mb-4.mb-lg-0
+              .bloque-texto-a__texto.p-4
+                p.mb-4 Conocer el cuerpo humano es sumamente importante para traducir su estructura en medidas y patronaje de prendas, por lo que, a continuación, le sugerimos hacer clic en el icono y observar el video “Estructura del cuerpo” (2020), donde podrá visualizar la división del cuerpo humano para tener en cuenta para su análisis de las medidas, ¿qué espera? Siga con su aprendizaje.
+            .col-lg-4.mb-lg-0
+              a(href='https://www.youtube.com/watch?v=V81Fjuq8XWs', target='_blank')
+                figure
+                  img.w-75.mx-auto(src="@/assets/curso/tema5/img07.svg", alt="clic para consultar sitio web") 
+    
+    Separador
+
+    #t5_1.titulo-segundo
+      h2 5.1 Análisis y clasificación de las medidas 
+    
+    .row.align-items-center.justify-content-center.mb-5
+      .col-8.col-lg-4.mb-4.mb-lg-0
+        figure
+          img.mx-auto(src="@/assets/curso/tema5/img08.png", alt="medidas")
+      .col-lg-8
+        p.mb-4 Para traducir a medidas el cuerpo humano, se debe guiar el proceso de patronaje para la elaboración de moldes, correspondientes a diseños o propuestas de la persona en el oficio de la creación de prendas de vestir, para esto es importante establecer el sistema de medidas que clasificadas denotan características para su uso e implementación, pero en esta medida debemos hacernos una pregunta ¿Qué es una medida? Son unidades numéricas que representan el tamaño de las figuras, personas y/u objetos. Por lo general se trabaja con un sistema de medida (cinta métrica) de manera lineal, para medir las diferentes partes del cuerpo.
+        p.mb-0 En Colombia la metodología implementada son centímetros o pulgadas. A continuación, se relacionan algunas consideraciones sobre las medidas:
+
+    .row.justify-content-center
+      .col-lg-10
+        .row.mb-5
+          .col-lg-6.mb-4
+            h4.mb-4 Las medidas sobre el cuerpo
+            p.mb-4 Por lo general se toman sobre los puntos de anclaje del cuerpo teniendo en cuenta los parámetros antropométricos y anatómicos, pasando por los volúmenes (contornos) protuberantes. Se clasifican en tres grupos.
+            .row.justify-content-center
+              .col-lg-11
+                ul.lista-ul--color
+                  li 
+                    .lista-ol--cuadro__vineta
+                      span 1
+                    | Contornos: se toman alrededor de la parte a medir, cerrando el metro.
+                  li 
+                    .lista-ol--cuadro__vineta
+                      span 2
+                    | Largos: se toman verticalmente y corresponden a largos y talles.
+                  li 
+                    .lista-ol--cuadro__vineta
+                      span 2
+                    | Anchos: se toman horizontalmente y corresponden a los anchos.
+          .col-lg-6
+            figure
+              img.mx-auto(src="@/assets/curso/tema5/img09.svg", alt="medidas sobre el cuerpo")
+        
+        .row.mb-4
+          .col-lg-7.mb-4
+            h4.mb-4 Clasificación y análisis de medidas anatómicas
+            p.mb-0 Esta clasificación está dada según la metodología que se empleará en patronaje para la construcción de prendas, por ello es importante diferenciarlas, para con ello saber qué medidas tomar o no, si es sobre el cliente o solo se utilizan cuadros de tallas estandarizadas. 
+          .col-lg-5
+            figure
+              img.mx-auto(src="@/assets/curso/tema5/img10.png", alt="Cinta metrica")
+    
+    Separador
+
+    #t5_2.titulo-segundo
+      h2 5.2 Análisis de medidas y cuadros de tallas
+    
+    p.mb-4 Los cuadros de tallas y medidas son usados dentro del sector para contar fácilmente con la información estandarizada de las tallas de un país o región, las cuales estarán determinadas de acuerdo a estudios antropométricos, dado que las condiciones del clima, entre otras hacen que la anatomía y estructura corporal puedan diferir. 
+
+    p.mb-5 Es importante también tener en cuenta que al hablar de tallas como ya se ha mencionado implica todo un estudio antropométrico, y esto está determinado por características agrupadas de acuerdo a ubicación geográfica, por ello es por lo que no todas las tallas pueden ser las mismas a nivel mundial, dado que difieren antropométricamente.
+
+    .row.justify-content-center
+      .col-lg-10
+        .bloque-texto-a.color-secundario.modBloque.p-4.p-md-5.mb-5
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-8.mb-4.mb-lg-0
+              .bloque-texto-a__texto.p-4
+                p.mb-4 A continuación, le sugerimos explorar la página web “Trekking Top” (s.f.), para que usted identifique las tallas y sus conversiones según la posición geográfica. Explore lo relacionado  con el calzado, la ropa de mujer y hombre, y niños.
+                p.mb-0 Revisa su sitio web haciendo clic en el icono.
+            .col-lg-4.mb-lg-0
+              a(href='https://trekkingtop.com/guia-tallas-columbia/', target='_blank')
+                figure
+                  img.w-75.mx-auto(src="@/assets/curso/tema5/img11.svg", alt="clic para consultar sitio web")
+        
+        p.mb-4 En el siguiente video “Toma de Medidas” (2020), se muestra cómo se procede con la medición de las partes del cuerpo. Es importante ubicar puntos referenciales en la estructura corporal de las personas para proceder con la toma de medidas ¿has tomado alguna vez medidas a un cuerpo?, sino lo has hecho percibe el video con atención.
+      
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/7tzquX5wI-4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          figcaption Video: Toma de medidas
+
 </template>
 
 <script>
--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -3,6 +3,107 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
+    .titulo-principal
+        .titulo-principal__numero
+          span 6
+        h1 Lanzamiento del producto de moda
+    
+    .row.align-items-center.justify-content-center.mb-5
+      .col-lg-8.mb-4.mb-lg-0
+        p.mb-4 Las empresas se mantienen en una constante lucha por diferenciarse en el mercado, manteniendo la identidad deseada y un público fidelizado, además de otros elementos que constituyen su desarrollo. Para ello, la generación de nuevos productos constituye una actividad importante para que las empresas o sus marcas no se pierdan de vista ante la infinidad de competencia comercial.
+        p.mb-0 Existen dos variables que obligan a las empresas a lanzar nuevos productos, según Rivera (2016) la primera es la competencia, debido a las continuas mejoras de productos con el fin de atraer una mayor cantidad de clientes e incrementar ventas, innovando procesos para optimizar recursos. Por eso, mientras una marca está segura con el éxito de un determinado producto, puede que otra haya hecho un estudio minucioso para generar otro producto mejorado que acoja un público mucho más grande. La segunda variable es el cliente y el mismo mercado, que cambia su percepción según el contexto y necesidades, con lo que la marca debe estar monitoreando constantemente la percepción del público frente a sus productos, definir una estrategia adecuada según los comportamientos del consumidor y elegir las mejores herramientas para generar nuevos productos que satisfagan a su público objetivo.
+      .col-lg-4
+        figure
+          img.mx-auto(src="@/assets/curso/tema6/img01.svg", alt="medidas")
+    
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-d.color-acento-contenido.p-4
+          .bloque-texto-d__texto.mb-2
+            i.fas.fa-quote-left
+            p Se debe tener muy claro que en el momento en que una marca del sector moda desarrolla un producto, no solo da a conocer un elemento de vestir al público externo, sino que ofrece una experiencia, una forma de ver el mundo y la manera en que quien viste dicho producto sea percibido por los demás. Esto supone una entrega de valor que va más allá del mero precio económico que un consumidor debe pagar por un producto del sector moda, se enfoca en la fibra emocional que toca el producto para quien lo adquiere, además del estatus que le ofrece la marca al obtener dicho elemento.
+            i.fas.fa-quote-right
+          .bloque-texto-d__autor 
+            .h5.mb-0 Posner, 2011
+    
+    p.mb-4 De esta forma, para lanzar y promocionar un producto, la marca puede enfocar su estrategia de marketing hacia los atributos de este, ya sea desde su proceso de producción, materiales o apoyo a una causa determinada, haciendo énfasis en los beneficios que el consumidor obtendrá.
+
+    Separador
+
+    #t6_1.titulo-segundo
+      h2 6.1 Planificación para lanzamiento de producto
+    
+    .row.align-items-center.justify-content-center.mb-4
+      .col-8.col-lg-4.mb-4.mb-lg-0
+        figure
+          img.mx-auto(src="@/assets/curso/tema6/img02.svg", alt="medidas")
+      .col-lg-8
+        p.mb-4 De acuerdo con Sordo (2020), para que una marca lance un producto es importante tener muy claro el tipo de mercado en el que se va a desenvolver, además de los objetivos que desea alcanzar, por ello es tan importante realizar un estudio previo para identificar en qué escenario se va a desarrollar la marca. Tan importante como conocer el mercado, es identificar la competencia, no para comparar lo propio sino para ver a qué se enfrenta la marca con relación a su distinción en el escenario comercial. Igualmente, la creación de espacios como grupos focales dentro de la empresa serán herramientas indispensables a la hora de tener en cuenta a los colaboradores que compartan diferentes puntos de vista con respecto al producto.
+    
+    p.mb-4 Por lo anterior, la comunicación juega un papel fundamental para promocionar el lanzamiento del producto, por ello, se deben establecer estrategias definidas utilizando los canales más adecuados según el público objetivo y los recursos que se tiene al alcance, en este punto, el trabajo con blogueros reconocidos, puede ser un factor diferenciador debido a la gran influencia que éstos tienen sobre el público objetivo de la marca, eso sí, se debe estudiar a qué tipo de persona va dirigido el producto para elegir el influenciador adecuado.
+
+    .cajon.color-primario.bg011.p-4.mb-4
+      p.mb-0 Del mismo modo, la atención al cliente será un factor diferenciador para que el público obtenga la información precisa y el trato adecuado para adquirir el producto, además se debe organizar un cronograma preciso con las fechas en que se realizará las actividades designadas por el equipo de trabajo de acuerdo con el plan establecido. Para concluir y como paso importante, se debe medir los resultados, estos permitirán identificar si las metas fueron alcanzadas o si por el contrario se debe modificar la estrategia.
+    
+    Separador
+
+    #t6_2.titulo-segundo
+      h2 6.2 Ciclo de vida del producto moda
+    
+    p.mb-4 Todos los productos que se lanzan al mercado presentan una serie de características según pasa el tiempo, lo que se denomina ciclo de vida del producto y en este se establecen una sucesión de fases que según Kotler & Keller (2012) determinarán factores representativos de cada una de ellas. Es de entender que el ciclo de vida del producto es continuo, puesto que mientras un elemento empieza a tener su apogeo, otro puede ser reemplazado por éste, de esta forma, cada fase que compone el ciclo será descrita desde la perspectiva de Bur (2013) teniendo en cuenta sus principales características:
+
+    .row.justify-content-center.mb-4
+      .col-lg-10
+        PasosA.color-secundario.mb-5(tipo="n")
+          .row.align-items-center
+            .col-md-10.mb-4.mb-md-0
+              h3 Fase 1. Introducción
+              p Se caracteriza por el lanzamiento del producto al mercado. Hay un número pequeño de conocedores de dicho producto y se podría considerar alto el costo del lanzamiento, ya que en este momento aún no se tiene en cuenta la inversión del diseño, la promoción y los elementos que permiten dar a conocer el producto.
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema6/img03.svg', alt='Fase 1')
+
+          .row.align-items-center
+            .col-md-2.mb-4.mb-md-0
+              figure
+                img(src='@/assets/curso/tema6/img04.svg', alt='Fase 2')
+            .col-md-10
+              h3 Fase 2. Crecimiento
+              p El producto se ha dado a conocer mucho más que en su introducción y las ventas empiezan a aumentar debido a la actividad del público influenciado por los primeros compradores. En esta fase aparecen las copias de los elementos del producto, para ser utilizados por la competencia con menor atención al detalle y calidad.
+          
+          .row.align-items-center
+            .col-md-10.mb-4.mb-md-0
+              h3 Fase 3. Madurez
+              p El producto se vende bastante y tiene una alta distribución, con esto se generan diversas variaciones en el estilo, además se ofrecen distintos precios según las necesidades del cliente. Al estar en una gran cantidad de medios de difusión, el producto moda es adoptado por grandes masas, siguiendo los patrones de los primeros y segundos compradores. 
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema6/img05.svg', alt='Fase 3')
+
+          .row.align-items-center
+            .col-md-2.mb-4.mb-md-0
+              figure
+                img(src='@/assets/curso/tema6/img06.svg', alt='Fase 4')
+            .col-md-10
+              h3 Fase 4. Declinación
+              p Cuando las ventas empiezan a descender y la distribución ya no se desarrolla en grandes cantidades, siendo selectiva. Del mismo modo, las personas que adquieren los productos en esta etapa son un grupo poblacional que no se interesan por las nuevas tendencias. 
+          
+          .row.align-items-center
+            .col-md-10.mb-4.mb-md-0
+              h3 Fase 5. Obsolescencia
+              p En el momento en que ya no queda casi población para adquirir el producto porque pasó de moda, es cuando se decide su descontinuación para acabar con las existencias 
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema6/img07.svg', alt='Fase 5')
+
+    .cajon.color-acento-contenido.bg02.p-4
+      p.mb-0 Al entender las oportunidades y amenazas que presenta un producto al ubicarlo en una determinada fase del ciclo de vida, es como permitirá identificar las mejores estrategias o herramientas para que dicho producto pueda mantenerse por mayor tiempo posible en el mercado (Kotler & Keller, 2012), el cual con un adecuado manejo podría llegar a ser un elemento clásico que se adapte a las distintas exigencias del tiempo y el mercado.
+    
+    
+
+
+        
+      
+
 </template>
 
 <script>
